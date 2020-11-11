5907aca41b402c8b9a9ded28ae1779354b201622
Anibal Schlegel <anisch@gmail.com>
2020-11-11 11:45:23 -0300

    Removed unused files

--- COME en CCL query.sql
+++
@@ -1,4 +0,0 @@
-﻿select cast(round(p.ClosingPrice/d.Price,2) as numeric(36,2)) from PriceData p 
-join Symbol s on p.Symbol_ID = s.ID 
-join DollarData d on p.Date = d.ExchangeDate
-where s.Name = 'COME' order by p.Date
